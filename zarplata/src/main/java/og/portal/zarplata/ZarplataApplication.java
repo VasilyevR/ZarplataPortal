500048737b7b10184e77241804a1efda104799c8
VasilyevR <romanvasilyev@gmail.com>
2024-02-26 21:51:26 +0300

    Init Spring project

---
+++ zarplata/src/main/java/og/portal/zarplata/ZarplataApplication.java
@@ -0,0 +1,13 @@
+package og.portal.zarplata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZarplataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZarplataApplication.class, args);
+	}
+
+}
---
+++ zarplata/src/test/java/og/portal/zarplata/ZarplataApplicationTests.java
@@ -0,0 +1,13 @@
+package og.portal.zarplata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZarplataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
