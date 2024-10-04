3c9b1045a47a4ab245d75bccf54398be8632348f
VasilyevR <romanvasilyev@gmail.com>
2024-10-04 18:29:51 +0300

    Add template

--- src/main/java/og/portal/zarplata/controller/HelloController.java
+++ src/main/java/og/portal/zarplata/controller/HelloController.java
@@ -1,16 +1,20 @@
 package og.portal.zarplata.controller;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.security.core.GrantedAuthority;
 import java.util.stream.Collectors;
 
-@RestController
+@Controller
 public class HelloController {
     @GetMapping("/auth")
-    public String auth(final Authentication auth) {
-        return String.format("Hello, %s. You have authorities: %s", auth.getPrincipal(),
-                auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", ")));
+    public String auth(Model model, final Authentication auth) {
+        String principal = auth.getPrincipal().toString();
+        String authorities = auth.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.joining(", "));
+        model.addAttribute("principal", principal);
+        model.addAttribute("authorities", authorities);
+        return "main/home";
     }
 }
