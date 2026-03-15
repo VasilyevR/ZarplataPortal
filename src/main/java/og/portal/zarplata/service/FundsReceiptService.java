890a8e26a3a36b6de1de542cf31fc19ca31a8299
VasilyevR <romanvasilyev@gmail.com>
2026-03-16 00:54:20 +0300

    Update Excel column name constants in FundsReceiptService

--- src/main/java/og/portal/zarplata/service/FundsReceiptService.java
+++ src/main/java/og/portal/zarplata/service/FundsReceiptService.java
@@ -32,10 +32,10 @@ import java.util.*;
 @RequiredArgsConstructor
 public class FundsReceiptService {
 
-    private static final String PAID_AMOUNT_COLUMN_NAME = "Оплаченная сумма";
-    private static final String PAYMENT_DATE_COLUMN_NAME = "Дата оплаты клиентом";
-    private static final String CLIENT_NAME_COLUMN_NAME = "Клиент";
-    private static final String ORDER_DATE_COLUMN_NAME = "Дата заказа";
+    private static final String PAID_AMOUNT_COLUMN_NAME = "Сумма заказа";
+    private static final String PAYMENT_DATE_COLUMN_NAME = "Дата прихода";
+    private static final String CLIENT_NAME_COLUMN_NAME = "Название фирмы";
+    private static final String ORDER_DATE_COLUMN_NAME = "Дата отгрузки";
     private static final String EXCEL_EXTENSION = ".xlsx";
 
     @Value("${app.share.folder.path}")
